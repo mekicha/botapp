631fda54bc135a1b4c6e9d18c32f733e7989009e
mekicha <emeka.icha@gmail.com>
2017-10-04 15:36:15 +0300

    (fix) handle response

--- webapp.go
+++ webapp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,23 +10,41 @@ import (
 )
 
 func main() {
+	
+	http.HandleFunc("/", handler)
+	
+	// http.HandleFunc("/" + bot.Token, botHandler)
+	fmt.Println("Listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		log.Fatal("Listen and Serve Error:")
+	}
 
+}
+
+func handler(res http.ResponseWriter, req *http.Request) {
 	bot, err := telebot.NewBot(os.Getenv("TOKEN"))
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Printf("Authorized on account %s", bot.Owner.Username)
 	updates := bot.ListenForWebhook("/" + bot.Token)
-
+	
 	for update := range updates {
-		log.Printf("%+v\n", update)
+		fmt.Fprintf(res, "%+v\n", update)
 	}
-	http.HandleFunc("/", handler)
-	fmt.Println("Listening...")
-	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-
 }
 
-func handler(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "Hello, this is our first Go web app on Heroku!")
-}
+// func botHandler(res http.ResponseWriter, req *http.Request) {
+// 	var update telebot.Update
+
+// 	decoder := json.NewDecoder(req.Body)
+
+// 	err := decoder.Decode(&update)
+// 	if err != nil {
+// 		log.Println( "Error decoding request body")
+// 	}
+// 	fmt.Fprintf(res, update)
+// }
